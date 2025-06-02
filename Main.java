592b6c1db053ff931f0b478aa1f2b0dc5275698d
prakashpc1 <87630077+prakashpc1@users.noreply.github.com>
2025-06-02 20:07:31 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 Welcome to Simple Java Task Manager");
+        TaskManager manager = new TaskManager();
+        manager.startMenu();
+    }
+}
